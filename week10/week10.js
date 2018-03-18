d807a3bad31d75734039320deb03f449b894b0ee
shadolite <amy.kawaii@gmail.com>
2018-03-17 23:20:36 -0600

    Added Javascript file.

---
+++ week10/week10.js
@@ -0,0 +1,10 @@
+﻿function loadData(fileName) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("demo").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", fileName + ".txt", true);
+    xhttp.send();
+}
